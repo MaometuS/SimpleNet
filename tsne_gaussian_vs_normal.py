e5983b22b67ab7f6ab11288fcf7e8e6d8dcf451a
Mukhammad Rakhimov <maometusu@gmail.com>
2026-05-09 22:16:58 +0900

    agg

--- tsne_gaussian_vs_normal.py
+++ tsne_gaussian_vs_normal.py
@@ -21,6 +21,8 @@ import os
 import click
 import torch
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
